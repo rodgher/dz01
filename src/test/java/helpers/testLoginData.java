362627b194d27cc47ad6d3c3fdb274cd55d38dc8
pvv <v.pomazunov@robo.finance>
2020-06-03 16:13:36 +0700

    change trueLogin

--- src/test/java/helpers/testLoginData.java
+++ src/test/java/helpers/testLoginData.java
@@ -2,7 +2,7 @@ package helpers;
 
 public class testLoginData {
 
-    public static String trueLogin = "tomsmith1";
+    public static String trueLogin = "tomsmith";
     public static String errorLogin = "tomsmith1";
     public static String truePassword = "SuperSecretPassword!";
     public static String errorPassword = "SuperSecretPassword!!";
